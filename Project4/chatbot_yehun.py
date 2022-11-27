58c42f94c1471d1755f03d9c2aa1894b19cd1ae2
gnsdp99 <rladpgns99@gmail.com>
2022-11-28 00:29:03 +0900

    update

--- Project4/chatbot_yehun.py
+++ Project4/chatbot_yehun.py
@@ -1,42 +1,54 @@
 import pandas as pd
 import re, time
-
+import random
 WORD_COL = 0
 RULE_COL = 1
 RESP_COL = 2
 
 RECIPE_START_ROW = 0 
-RECIPE_END_ROW = 14
+RECIPE_END_ROW = 17
 INGREDIENT_START_ROW = 0
 INGREDIENT_END_ROW = 10
 TYPE_START_ROW = 0
-TYPE_END_ROW = 6
+TYPE_END_ROW = 7
+
+df_recipe = pd.read_excel("stew.xlsx")
+df_ingredient = ""
+df_type = pd.read_excel("type.xlsx")
 
 # 1. 사용자가 기능을 선택한다.
 # 2. 해당 기능에 해당하는 키워드를 입력하면 답변한다.
 
-def keyword_check(df, com, start_len, end_len): ### 동일한 이름이 들어간 음식을 구분하도록 ex)김치찌개, 참치 김치찌개
+def keyword_check(df, com, rule, start_len, end_len): ### 동일한 이름이 들어간 음식을 구분하도록 ex)김치찌개, 참치 김치찌개
     matched_len = 0
     response = ""
+    response_list = []
     for i in range(start_len, end_len):
         compare_word = df.values[i][WORD_COL]
-        search = re.search(compare_word, "".join(com))
-        if search != None:
-            span = search.span()
-            if matched_len < span[1] - span[0]:
-                matched_len = span[1] - span[0]
-                response = df.values[i][RESP_COL]
+        com = "".join(com)
+        if rule == 1:
+            search = re.search(compare_word, com)
+            if search != None:
+                span = search.span()
+                if matched_len < span[1] - span[0]:
+                    matched_len = span[1] - span[0]
+                    response = df.values[i][RESP_COL]
+    
+        elif rule == 3:
+            num_list = [i for i in range(RECIPE_START_ROW, RECIPE_END_ROW+1)]
+            if com in compare_word:
+                random_list = random.sample(num_list, 5)
+                for i in random_list:
+                    response_list.append(df_recipe.values[i][WORD_COL])      
     if response:
         return True, response
+    elif response_list:
+        return True, response_list
     else:
         return False, None
 
 # pandas 엑셀의 값을 넣으면 df에 저장됨.
-df_recipe = pd.read_excel("stew.xlsx")
-df_ingredient = ""
-df_type = pd.read_excel("type.xlsx")
-print(df_recipe.values[13])
-print(df_type.values[6])
+
 ##################################################
 # 룰을 리스트에 저장 
 # 지금은 레시피와 관련된 룰만 하지만 
@@ -68,19 +80,19 @@ if "종료" in command: ### 종료 조건
 
 if command == "1":
     print("챗봇: 어떤 레시피를 알려드릴까요?")
-    rule = rule_1
+    rule = 1
     df = df_recipe
     START_ROW = RECIPE_START_ROW
     END_ROW = RECIPE_END_ROW
 elif command == "2":
     print("챗봇: 먹고 싶은 메인 재료를 입력하세요!")
-    rule = rule_2
+    rule = 2
     df = df_ingredient
     START_ROW = INGREDIENT_START_ROW
     END_ROW = INGREDIENT_END_ROW
 elif command == "3":
     print("챗봇: 먹고 싶은 요리 종류를 입력하세요!")
-    rule = rule_3
+    rule = 3
     df = df_type
     START_ROW = TYPE_START_ROW
     END_ROW = TYPE_END_ROW
@@ -89,9 +101,13 @@ command = list(input("나: ").split())
 command_non_space = list("".join(command))
 print()
 
-keyword_flag, response = keyword_check(df, command_non_space, START_ROW, END_ROW)
+keyword_flag, response = keyword_check(df, command_non_space, rule, START_ROW, END_ROW)
 while keyword_flag == False:
-    keyword_flag, response = keyword_check(df, command_non_space, START_ROW, END_ROW)
-    
-print(response)
+    keyword_flag, response = keyword_check(df, command_non_space, rule, START_ROW, END_ROW)
+
+if rule == 1:
+    print(response)
+elif rule == 3:
+    for i, name in enumerate(response):
+        print(i+1, ":", name)
 print()
